b5595eae8af9dceed68336867c8ba025d204770d
Chris Young <chrisyoung0101@gmail.com>
2022-12-14 18:10:13 -0600

    end video 7

--- components/Header.js
+++ components/Header.js
@@ -17,20 +17,34 @@ const Logo = styled.h1`
   }
 `;
 
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
         <Logo>
-          <Link href="/">
-            <h1>Sick Fits</h1>
-          </Link>
+          <Link href="/">Sick Fits</Link>
         </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
