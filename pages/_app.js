7781be1f222b85c7ea7d0e60b390b659a4431e29
Chris Young <chrisyoung0101@gmail.com>
2022-12-19 06:39:52 -0600

    Add apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import NProgress from 'nprogress';
 // we need router to hook into a couple events from nprogress
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 // I AM THE HIGHEST LEVEL COMPONENT
 
 // comes from nprogress - use this for initial set up before custom styling
 // import 'nprogress/nprogress.css';
+// this is our custom style
 import '../components/styles/nprogress.css';
 
 // when routeChangeStart starts, animate the start
@@ -18,10 +21,25 @@ Router.events.on('routeChangeError', () => NProgress.done());
 // here we are wrapping all pages in this MyApp component aka __app
 // often need to restart project when adding such a change for it to start working
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+// Tell next.js go fetch all the queries that are in the children components
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
