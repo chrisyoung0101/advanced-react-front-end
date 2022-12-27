aef76e18d22d340d23da10e6f2d50c680ee7932d
Chris Young <chrisyoung0101@gmail.com>
2022-12-27 15:15:21 -0600

    commit this

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b5a47fc0139bfd1ba2c1"; }
+/******/ 		__webpack_require__.h = function() { return "ed1c068007406b1bdf7f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,45 +1,119 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import PriceTag from './styles/PriceTag';
+import DisplayError from './ErrorMessage';
+
+// We take the user form input data, add them to the vars below and then create it in the BE in keystone
+// aka take the data from the user form and create a new product in keystone.
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in?  And what are their types?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   // using curly braces because we are returning an object from useForm() custom hook
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'Nice Hat',
     price: 23424,
     description: 'This is so fabulous!',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs, // this is passing in values from $name, $description, $price, $image
+    }
+  );
+
+  console.log(createProduct);
+
   return (
-    <form>
-      <label htmlFor="name">
-        Name
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Name"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input
-          type="number"
-          id="price"
-          name="price"
-          placeholder="Price"
-          value={inputs.price}
-          onChange={handleChange}
-        />
-      </label>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        console.log(inputs);
+        // Submit the input fields to the backend:
+        // createProduct() function has already been loaded with our user data from variables: inputs,
+        // so... you can pass them like above (for when you already know the values at the time of submission) or
+        // directly in the createProduct() function when you don't know the values ahead of time.
+        const res = await createProduct();
+        console.log(res);
+      }}
+    >
+      {/* see the actual ErrorMessage.js component which is named  DisplayError here because that is how we exported it. */}
+      <DisplayError error={error} />
+      {/* if in loading state, disable the form with disabled={loading} */}
+      {/*  when in a loading state this shows the progress bar : aria-busy={loading} */}
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Price
+          <textarea
+            type="text"
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+      </fieldset>
 
-      <button type="button" onClick={clearForm}>
-        Clear Form
-      </button>
-
-      <button type="button" onClick={resetForm}>
-        Reset Form
-      </button>
-    </form>
+      <button type="submit">+ Add Product</button>
+    </Form>
   );
 }
