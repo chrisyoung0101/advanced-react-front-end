aaf0cddc4b7cff518183a65f1d09f574064689ec
Chris Young <chrisyoung0101@gmail.com>
2023-01-11 07:08:07 -0600

    video 37

---
+++ components/SignIn.js
@@ -0,0 +1,70 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    ... on UserAuthenticationWithPasswordSuccess {
+      item {
+        id
+        email
+        name
+      }
+    }
+  }
+`;
+
+export default function SignIn() {
+  // syncing the form data to state
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+
+  // call the mutation
+  const [signin, { error, loading }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    // refetch the currently logged in user
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault(); // stop form from submitting
+    console.log(inputs);
+    // Send email & password to the graphql API
+  }
+
+  return (
+    // POST method will not put the PW in the URL.  Also don't log out the body of the request because it will send it over to the server.
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Into Your Account</h2>
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+}
