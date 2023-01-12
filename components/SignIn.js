9e121276f6df1be4878e65101e4a1af8a617fd88
Chris Young <chrisyoung0101@gmail.com>
2023-01-12 07:05:59 -0600

    end v 37

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 import Form from './styles/Form';
 import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
+import Error from './ErrorMessage';
 
 // this is the mutation var SIGNIN_MUTATION
 const SIGNIN_MUTATION = gql`
@@ -16,6 +17,10 @@ const SIGNIN_MUTATION = gql`
           name
         }
       }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
+      }
     }
   }
 `;
@@ -27,8 +32,8 @@ export default function SignIn() {
     password: '',
   });
 
-  // call the mutation
-  const [signin, { error, loading }] = useMutation(SIGNIN_MUTATION, {
+  // call the mutation -> note : look at data by either destructuring from useMutation or below with signin()
+  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
     variables: inputs,
     // refetch the currently logged in user
     refetchQueries: [{ query: CURRENT_USER_QUERY }],
@@ -39,13 +44,24 @@ export default function SignIn() {
     console.log(inputs);
     const res = await signin();
     console.log(res);
+    // clear form after submitting
+    resetForm();
     // Send email & password to the graphql API
   }
+
+  // return the response data?.authenticateUserWithPassword or nothing
+  const error =
+    data?.authenticateUserWithPassword.__typename ===
+    'UserAuthenticationWithPasswordFailure'
+      ? data?.authenticateUserWithPassword
+      : undefined;
 
   return (
     // POST method will not put the PW in the URL.  Also don't log out the body of the request because it will send it over to the server.
     <Form method="POST" onSubmit={handleSubmit}>
       <h2>Sign Into Your Account</h2>
+      {/* Error component only renders if all are true */}
+      <Error error={error} />
       <fieldset>
         <label htmlFor="email">
           Email
